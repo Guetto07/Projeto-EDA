54a6176d511febcb366e62963fb34c16fb17360d
Guetto07 <a26019@alunos.ipca.pt>
2023-06-01 10:44:16 +0100

    Remover versão anterior da 2FASE

--- 2FASE/listar.h
+++
@@ -1,8 +0,0 @@
-#ifndef LISTAR_H
-#define LISTAR_H
-
-#include "grafo.h"
-
-void listarMeiosNoRaio(Grafo* grafo, double latitude, double longitude, int tipoMeio, double raio);
-
-#endif  // LISTAR_H
